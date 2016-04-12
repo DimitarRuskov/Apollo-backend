e9901251069541dc564dfc0c303fd01ff63acd11
Deyvid Raychev <Deyvid.Raychev@Softwaregroup-bg.com>
2016-04-12 10:34:14 +0300

    minor improvement

--- config/koa.js
+++ config/koa.js
@@ -4,25 +4,21 @@ const MongoStore = require('koa-generic-session-mongo');
 const logger = require('koa-logger');
 const compress = require('koa-compress');
 const serve = require('koa-static');
-// const errorHandler = require('koa-error');
 const bodyParser = require('koa-bodyparser');
 const cors = require('koa-cors');
 const path = require('path');
 
 module.exports = function(app, config, passport) {
     app.keys = config.app.keys;
-    app.use(serve(path.join(config.app.root, 'static')));
+    app.use(serve(path.join(config.app.root, 'public')));
     
     app.use(cors());
 
-    if (config.app.env !== 'test') {
+    if (config.app.env !== 'development') {
         app.use(logger());
     }
 
-    // app.use(errorHandler());
-
     app.use(session({
-        key: 'test',
         store: new MongoStore({ url: config.mongo.url })
     }));
 
