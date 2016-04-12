79c90b6e704ad06f687a346a571e6753405c5ccf
Deyvid Raychev <Deyvid.Raychev@Softwaregroup-bg.com>
2016-04-12 10:06:41 +0300

    minor refactoring and recipe service add/list functionality

--- api/services/category.js
+++ api/services/category.js
@@ -1,28 +1,34 @@
 'use strict';
-var passport = require('koa-passport');
 var Category = require('mongoose').model('Category');
 
 exports.listCategories = function * (_this) {
     try {
         var params = _this.request.body.params || {};
         var categories = yield Category.find().sort(params.orderBy || {});
+        _this.status = 200;
+        _this.body = { categories: categories };
     } catch (err) {
         throw err;
     }
-
-    _this.status = 200;
-    _this.body = { categories: categories };
 };
 
 exports.createCategory = function * (_this) {
     try {
         var params = _this.request.body.params;
-        var category = new Category({ name: params.name, description: params.description, updatedAt: new Date() });
+        var creationDate = new Date();
+        
+        var category = new Category({
+            name: params.name,
+            description: params.description,
+            createdAt: creationDate,
+            updatedAt: creationDate
+        });
+        
         category = yield category.save();
+        
+        _this.status = 200;
+        _this.body = { category: category };
     } catch (err) {
         throw err;
     }
-
-    _this.status = 200;
-    _this.body = { category: category };
 };
--- core/models/category.js
+++ core/models/category.js
@@ -1,14 +1,13 @@
 'use strict';
 
-var bcrypt = require('../../lib/bcrypt-thunk');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var co = require('co');
 
 var CategorySchema = new Schema({
     name: {type: String, required: true, unique: true},
     description: {type: String, required: true},
-    updatedAt: {type: Date, required: true}
+    createdAt: {type: Date, required: true},
+    updatedAt: {type: Date, required: false}
 });
 
 mongoose.model('Category', CategorySchema);
