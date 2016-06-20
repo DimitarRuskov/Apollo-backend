3780ed2caa38587e726c7ba7ac589178ea0a8ff1
Deyvid Raychev <Deyvid.Raychev@Softwaregroup-bg.com>
2016-06-20 10:09:32 +0300

    queryParser middleware minor bug fix

--- api/middlewares/queryParser.js
+++ api/middlewares/queryParser.js
@@ -1,7 +1,10 @@
 var qs = require('qs');
 
 module.exports = function * queryParser(next) {
-    this.originalUrl = this.originalUrl.replace(this.req._parsedUrl.search, '');
-    this.query = qs.parse(this.req._parsedUrl.query);
+    if (this.req._parsedUrl && this.req._parsedUrl.search !== '') {
+        this.originalUrl = this.originalUrl.replace(this.req._parsedUrl.search, '');
+        this.query = qs.parse(this.req._parsedUrl.query);
+    }
+    
     yield next;
 };
